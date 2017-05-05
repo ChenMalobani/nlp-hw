8eb0e7397823d13b503c386b0e27600813ee66ec
irejwan <idanrejwan91@gmail.com>
2017-05-05 20:03:40 +0300

    fixed bug in Viterbi

--- hw3/hmm.py
+++ hw3/hmm.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from data import *
 import numpy as np
 
@@ -26,51 +27,80 @@ def hmm_train(sents):
     return total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, e_word_tag_counts,e_tag_counts
 
 
-def log_parameters_estimation(total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, e_word_tag_counts,e_tag_counts, lambda1=0.4, lambda2=0.4, lambda3=0.2):
-    transitions = {}
-    emissions = {}
-    for curr, before, last in q_tri_counts.keys():
-        transitions[(curr, before, last)] =\
-            np.log(lambda1 * q_tri_counts[(curr, before, last)] / q_bi_counts.get((before, last), np.inf) +
-                   lambda2 * q_bi_counts.get((curr, before), 0)/ q_uni_counts.get((before), np.inf) +
-                   lambda3 * q_uni_counts.get(curr, 0)/ total_tokens)
-    for word, tag in e_word_tag_counts.keys():
-        emissions[(word, tag)] = np.log(e_word_tag_counts[(word, tag)] / e_tag_counts.get(tag, np.inf))
-        return transitions, emissions
+# def log_parameters_estimation(total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, e_word_tag_counts,e_tag_counts, lambda1=0.4, lambda2=0.4, lambda3=0.2):
+#     transitions = {}
+#     emissions = {}
+#     for curr, before, last in q_tri_counts.keys():
+#         transitions[(curr, before, last)] =\
+#             np.log(lambda1 * q_tri_counts[(curr, before, last)] / q_bi_counts.get((before, last), np.inf) +
+#                    lambda2 * q_bi_counts.get((curr, before), 0)/ q_uni_counts.get((before), np.inf) +
+#                    lambda3 * q_uni_counts.get(curr, 0)/ total_tokens)
+#     for word, tag in e_word_tag_counts.keys():
+#         emissions[(word, tag)] = np.log(e_word_tag_counts[(word, tag)] / e_tag_counts.get(tag, np.inf))
+#     return transitions, emissions
 
 
-def hmm_viterbi(sent, total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, e_word_tag_counts,e_tag_counts):
+def calc_transition(total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, curr, before, last, lambda1=0.4, lambda2=0.4, lambda3=0.2):
+    transition =\
+        np.log(lambda1 * q_tri_counts.get((curr, before, last), 0) / q_bi_counts.get((before, last), np.inf) +
+               lambda2 * q_bi_counts.get((curr, before), 0)/ q_uni_counts.get((before), np.inf) +
+               lambda3 * q_uni_counts.get(curr, 0)/ total_tokens)
+    return transition
+
+
+def calc_emission(e_word_tag_counts, e_tag_counts, curr, tag):
+    return np.log(e_word_tag_counts.get((curr, tag), 0) / e_tag_counts.get(tag, np.inf))
+
+
+def hmm_viterbi(sent, total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, e_word_tag_counts, e_tag_counts):
     """
         Receives: a sentence to tag and the parameters learned by hmm
         Rerutns: predicted tags for the sentence
     """
     predicted_tags = [""] * (len(sent))
     ### YOUR CODE HERE
-    older  = ('DONTCARE','*')
-    transitions, emissions = log_parameters_estimation(total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, e_word_tag_counts,e_tag_counts)
+    older = ('DONTCARE','*')
+    # transitions, emissions = log_parameters_estimation(total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, e_word_tag_counts,e_tag_counts)
     tags = e_tag_counts.keys() + ['*']
     pi = np.empty((len(sent)+1, len(tags), len(tags)))
     bp = np.empty((len(sent)+1, len(tags), len(tags)))
     pi[0,tags.index(older[1]), tags.index(older[1])] = 0
+    s_k_before = ['*']
+    s_k_before_2 = ['*']
     for k in range(1, len(sent)+1):
-        if k == 1:
-            s_k_before = ['*']
-        else:
+        if k > 1:
             s_k_before = tags
-        if k <= 2:
-            s_k_before_2 = ['*']
-        else:
+        if k > 2:
             s_k_before_2 = tags
         for u in s_k_before:
             for v in tags:
-                a = np.array([pi[k-1, tags.index(w), tags.index(u)] + transitions.get((v, w, u),0) + emissions.get((sent[k-1][0],v),0) for w in s_k_before_2])
+                transitions = {w: calc_transition(total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, v, u, w) for w in s_k_before_2}
+                emission = calc_emission(e_word_tag_counts, e_tag_counts, sent[k-1][0], v)
+                a = np.array([pi[k-1, tags.index(w), tags.index(u)] + transitions[w] + emission for w in s_k_before_2])
                 pi[k, tags.index(u), tags.index(v)] = np.max(a)
                 bp[k, tags.index(u), tags.index(v)] = np.argmax(a)
-    predicted_tags[len(sent)-2] = tags[np.argmax(np.array([pi[len(sent), tags.index(m), tags.index(n)] + transitions.get(('STOP', m, n), 0) for m in tags for n in tags]))]
-    predicted_tags[len(sent)-1] = tags[np.argmax(np.array([pi[len(sent), tags.index(predicted_tags[len(sent)-2]), tags.index(m)] + transitions.get(('STOP', predicted_tags[len(sent)-2], m), 0) for m in tags]))]
 
-    for k in range(len(sent)-3, 0, -1):
-        predicted_tags[k] = tags[np.int(bp[k+2,tags.index(predicted_tags[k+1]),tags.index(predicted_tags[k+2])])]
+    # Prediction tags for the last 2 words in sent
+    transitions = {(u, v): calc_transition(total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, 'STOP', u, v)
+                  for u in tags for v in tags}
+
+    best_val = pi[len(sent), 0, 0] + transitions[(tags[0], tags[0])]
+    tag_n, tag_n_before = tags[0], tags[0]
+    for u in tags:
+        for v in tags:
+            curr_val = pi[len(sent), tags.index(u), tags.index(v)] + transitions[v, u]
+            if curr_val > best_val:
+                tag_n_before = u
+                tag_n = v
+                best_val = curr_val
+
+    predicted_tags[len(sent) - 2] = tag_n_before
+    predicted_tags[len(sent) - 1] = tag_n
+
+    # Prediction tags for all other words in sent
+    for k in range(len(sent) - 3, -1, -1):
+        predicted_tags[k] = tags[
+            np.int(bp[k + 3, tags.index(predicted_tags[k + 1]), tags.index(predicted_tags[k + 2])])]
     ### END YOUR CODE
     return predicted_tags
 
@@ -79,16 +109,17 @@ def hmm_eval(test_data, total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, e
     Receives: test data set and the parameters learned by hmm
     Returns an evaluation of the accuracy of hmm
     """
-    acc_viterbi = 0, 0
+    acc_viterbi = 0
+    total_token = 0
     ### YOUR CODE HERE
     for sent in test_data:
         predicted_tags = hmm_viterbi(sent, total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, e_word_tag_counts,e_tag_counts)
         label_tags = [tup[1] for tup in sent]
-        if label_tags == predicted_tags:
-            acc_viterbi[1] += 1
-        acc_viterbi[0] += 1
+        compare = [(label_tags[i] == predicted_tags[i]) for i in range(len(sent))]
+        acc_viterbi += sum(compare)
+        total_token += len(compare)
     ### END YOUR CODE
-    return acc_viterbi
+    return acc_viterbi / total_token
 
 if __name__ == "__main__":
     train_sents = read_conll_pos_file("Penn_Treebank/train.gold.conll")
@@ -100,11 +131,11 @@ if __name__ == "__main__":
 
     total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, e_word_tag_counts, e_tag_counts = hmm_train(train_sents)
     acc_viterbi = hmm_eval(dev_sents, total_tokens, q_tri_counts, q_bi_counts, q_uni_counts, e_word_tag_counts,e_tag_counts)
-    print "dev: acc hmm viterbi: " + acc_viterbi
+    print "dev: acc hmm viterbi: " + str(acc_viterbi)
 
     if os.path.exists("Penn_Treebank/test.gold.conll"):
         test_sents = read_conll_pos_file("Penn_Treebank/test.gold.conll")
         test_sents = preprocess_sent(vocab, test_sents)
         acc_viterbi = hmm_eval(test_sents, total_tokens, q_tri_counts, q_bi_counts, q_uni_counts,
                                            e_word_tag_counts, e_tag_counts)
-        print "test: acc hmm viterbi: " + acc_viterbi
+        print "test: acc hmm viterbi: " + str(acc_viterbi)
