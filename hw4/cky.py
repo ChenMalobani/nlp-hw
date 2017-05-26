cee79f5f60d840ba906bf0fe9c634ae12ff977d1
Avi Caciularu <avi.c33@gmail.com>
2017-05-26 19:29:37 +0300

    CKY

--- hw4/cky.py
+++ hw4/cky.py
@@ -1,5 +1,8 @@
+from __future__ import division
 from PCFG import PCFG
 import math
+import numpy as np
+import collections
 
 def load_sents_to_parse(filename):
     sents = []
@@ -10,12 +13,53 @@ def load_sents_to_parse(filename):
                 sents.append(line)
     return sents
 
+def get_log_probability(pcfg,symbol,token):
+    for item in pcfg._rules[symbol]:
+        if item[0][0] == token:
+            return np.log(item[1] / pcfg._sums[symbol])
+    return -np.inf
+
+def get_cky_parse_tree(sent, bp, i, j, symbol):
+    if i == j:
+        return "(" + symbol + " " + sent[i] + ")"
+    left, right, split_index = bp[i][j][symbol]
+
+    return "(" + symbol + " " + get_cky_parse_tree(sent, bp, i, split_index, left) + " " \
+                        + get_cky_parse_tree(sent, bp, split_index + 1, j, right) + ")"
+
 def cky(pcfg, sent):
     ### YOUR CODE HERE
-    raise NotImplementedError
-    ### END YOUR CODE
-    return "FAILED TO PARSE!"
+    tokens = sent.split(' ')
+    pi = collections.defaultdict(lambda: collections.defaultdict(dict))
+    bp = collections.defaultdict(lambda: collections.defaultdict(dict))
+    for i in range(1,len(tokens)+1):
+        for rule in pcfg._rules.keys():
+            pi[i][i][rule] = get_log_probability(pcfg,rule,tokens[i-1])
 
+    for i in range(1, len(tokens)):
+        for l in range(1, len(tokens)):
+            j = i + l
+            max_val = -np.inf
+            max_ind = None
+            for x in pcfg._rules.keys():
+                sum_weight = pcfg._sums[x]
+                for rhs in pcfg._rules[x]:
+                    if not pcfg.is_preterminal(rhs[0]):
+                        for s in range(i, j):
+                            rule = rhs[0]
+                            y, z = rule
+                            prob = np.log(rhs[1]/sum_weight)
+                            val = prob + pi[i][s].get(y,-np.inf) + pi[s+1][j].get(z,-np.inf)
+                            if val > max_val:
+                                max_val = val
+                                max_ind = (y,z,s)
+                pi[i][j][x] = max_val
+                bp[i][j][x] = max_ind
+
+        if pi[0][len(sent)-1]["ROOT"] == -np.inf:
+            return "FAILED TO PARSE!"
+
+    return get_cky_parse_tree(sent, bp, 0, len(sent) - 1, "ROOT")
 if __name__ == '__main__':
     import sys
     pcfg = PCFG.from_file_assert_cnf(sys.argv[1])
